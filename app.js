af0adf072647b1dd818309794255a1b2bfa7b007
Paul Rafter <paulrafter@hotmail.com>
2020-05-10 22:13:01 +0100

    update

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ var activity    		= require('./routes/activity');
 var urlencodedparser 	= bodyParser.urlencoded({extended:false});
 var app 				= express();
 var local       		= false;
-const soapRequest 		= require('easy-soap-request');
 
 
 // access Heroku variables
@@ -22,12 +21,8 @@ if ( !local ) {
 	  authUrl: 									process.env.authUrl,
 	  clientId: 								process.env.clientId,
 	  clientSecret: 							process.env.clientSecretSOAP,
-	  clientIdSOAP: 							process.env.clientIdSOAP,
-	  clientSecretSOAP: 						process.env.clientSecret,
-	  SOAPUri: 									process.env.SOAPUri,
 	  restUrl: 									process.env.restUrl,
 	  appUrl: 									process.env.baseUrl,
-	  promotionsListDataExtension: 				process.env.promotionsListDataExtension,
 	  controlGroupsDataExtension: 				process.env.controlGroupsDataExtension,
 	  updateContactsDataExtension: 				process.env.updateContactsDataExtension,
 	  insertDataExtension: 						process.env.insertDataExtension,
@@ -36,12 +31,8 @@ if ( !local ) {
 }
 
 // url constants
-const promotionsUrl 			= marketingCloud.restUrl + "data/v1/customobjectdata/key/" 	+ marketingCloud.promotionsListDataExtension 		+ "/rowset'";
-const globalCodesUrl 			= marketingCloud.restUrl + "data/v1/customobjectdata/key/" 	+ marketingCloud.globalVoucherPot 					+ "/rowset";
 const controlGroupsUrl 			= marketingCloud.restUrl + "data/v1/customobjectdata/key/" 	+ marketingCloud.controlGroupsDataExtension 		+ "/rowset";
 const updateContactsUrl 		= marketingCloud.restUrl + "data/v1/customobjectdata/key/" 	+ marketingCloud.updateContactsDataExtension 		+ "/rowset";
-const voucherPotsUrl 			= marketingCloud.restUrl + "data/v1/customobjectdata/key/" 	+ marketingCloud.voucherPotsDataExtension 			+ "/rowset";
-
 
 // Configure Express master
 app.set('port', process.env.PORT || 3000);
@@ -75,37 +66,40 @@ const getOauth2Token = () => new Promise((resolve, reject) => {
 	});
 });
 
-// configure FUEL SDK
-var FuelSoap = require('fuel-soap');
-
-const soapCreateQuery = () => new Promise((resolve, reject) => {
+const addQueryActivity = (payload) => new Promise((resolve, reject) => {
 
 	getOauth2Token().then((tokenResponse) => {
 
 		console.dir("Oauth Token");
 		console.dir(tokenResponse);
-
-		// example data
-		const url = 'https://graphical.weather.gov/xml/SOAP_server/ndfdXMLserver.php';
-		const sampleHeaders = {
-		  'user-agent': 'sampleTest',
-		  'Content-Type': 'text/xml;charset=UTF-8',
-		  'soapAction': 'https://graphical.weather.gov/xml/DWMLgen/wsdl/ndfdXML.wsdl#LatLonListZipCode',
-		};
-		const xml = fs.readFileSync('test/zipCodeEnvelope.xml', 'utf-8');
-
-		// usage of module
-		(async () => {
-		  const { response } = await soapRequest({ url: url, headers: sampleHeaders, xml: xml, timeout: 1000 }); // Optional timeout parameter(milliseconds)
-		  const { headers, body, statusCode } = response;
-		  console.log(headers);
-		  console.log(body);
-		  console.log(statusCode);
-		})();
+	var communicationCellData = {
+			"not_control": {
+		    	"cell_code"					: payload["cell_code"],
+		    	"cell_name"					: payload["cell_name"],
+		        "campaign_name"				: payload["campaign_name"],
+		        "campaign_id"				: payload["campaign_id"],
+		        "campaign_code"				: payload["campaign_code"],
+		        "cell_type"					: "1",
+		        "channel"					: "2",
+		        "is_putput_flag"			: "1"				
+			}
+	};
+	   	axios({
+			method: 'post',
+			url: targetUrl,
+			headers: {'Authorization': tokenResponse},
+			data: insertPayload
+		})
+		.then(function (response) {
+			console.dir(response.data);
+			return resolve(response.data);
+		})
+		.catch(function (error) {
+			console.dir(error);
+			return reject(error);
+		});
 
 	})
-
-
 });
 
 // insert data into data extension
@@ -113,14 +107,12 @@ app.get('/automation/create/query', async function (req, res){
 	console.dir("Dump request body");
 	console.dir(req.body);
 	try {
-		await soapCreateQuery();
+		await addQueryActivity(req.body);
 	} catch(err) {
 		console.dir(err);
 	}
 	
 });
-
-
 
 const getIncrements = () => new Promise((resolve, reject) => {
 	getOauth2Token().then((tokenResponse) => {
@@ -182,72 +174,7 @@ const saveToDataExtension = (targetUrl, payload, key, dataType, keyName) => new 
 				return reject(error);
 			});
 		})	
-	} else if ( dataType == "communication_cell") {
-		var insertPayload = [{
-	        "keys": {
-	            [keyName]: (parseInt(key) + 1)
-	        },
-	        "values": payload.control,
-
-    	},
-    	{
-	        "keys": {
-	            [keyName]: (parseInt(key) + 2)
-	        },
-	        "values": payload.not_control,
-	        
-    	}];
-    	console.dir(insertPayload);
-
-		getOauth2Token().then((tokenResponse) => {
-		   	axios({
-				method: 'post',
-				url: targetUrl,
-				headers: {'Authorization': tokenResponse},
-				data: insertPayload
-			})
-			.then(function (response) {
-				console.dir(response.data);
-				return resolve(response.data);
-			})
-			.catch(function (error) {
-				console.dir(error);
-				return reject(error);
-			});
-		})
-	} else if ( dataType == "promotion_description") {
-
-		var insertPayload = [];
-
-		for ( var i = 1; i <= Object.keys(payload.promotions).length; i++ ) {
-			insertPayload.push({
-		        "keys": {
-		            [keyName]: (parseInt(key) + i)
-		        },
-		        "values": payload.promotions["promotion_" + i],
-
-	    	});
-		}
-		console.dir("Promo desc data: ");
-		console.dir(insertPayload);
-
-		getOauth2Token().then((tokenResponse) => {
-		   	axios({
-				method: 'post',
-				url: targetUrl,
-				headers: {'Authorization': tokenResponse},
-				data: insertPayload
-			})
-			.then(function (response) {
-				console.dir(response.data);
-				return resolve(response.data);
-			})
-			.catch(function (error) {
-				console.dir(error);
-				return reject(error);
-			});
-		})
-	}
+	} 
 });
 
 const updateIncrements = (targetUrl, promotionObject, communicationCellObject, mcUniquePromotionObject, numberOfCodes) => new Promise((resolve, reject) => {
@@ -321,48 +248,6 @@ app.get("/dataextension/lookup/increments", (req, res, next) => {
 	})
 });
 
-//Fetch rows from promotions data extension
-app.get("/dataextension/lookup/promotions", (req, res, next) => {
-
-	getOauth2Token().then((tokenResponse) => {
-
-		axios.get(promotionsUrl, { 
-			headers: { 
-				Authorization: tokenResponse
-			}
-		})
-		.then(response => {
-			// If request is good... 
-			res.json(response.data);
-		})
-		.catch((error) => {
-		    console.dir("Error getting promotions");
-		    console.dir(error);
-		});
-	})	
-});
-
-//Fetch rows from promotions data extension
-app.get("/dataextension/lookup/globalcodes", (req, res, next) => {
-
-	getOauth2Token().then((tokenResponse) => {
-
-		axios.get(globalCodesUrl, { 
-			headers: { 
-				Authorization: tokenResponse
-			}
-		})
-		.then(response => {
-			// If request is good... 
-			res.json(response.data);
-		})
-		.catch((error) => {
-		    console.dir("Error getting global code");
-		    console.dir(error);
-		});
-	})		
-});
-
 //Fetch rows from control group data extension
 app.get("/dataextension/lookup/controlgroups", (req, res, next) => {
 
@@ -407,49 +292,6 @@ app.get("/dataextension/lookup/updatecontacts", (req, res, next) => {
 
 });
 
-//Fetch rows from voucher data extension
-app.get("/dataextension/lookup/voucherpots", (req, res, next) => {
-
-	getOauth2Token().then((tokenResponse) => {
-
-		axios.get(voucherPotsUrl, { 
-			headers: { 
-				Authorization: tokenResponse
-			}
-		})
-		.then(response => {
-			// If request is good... 
-			res.json(response.data);
-		})
-		.catch((error) => {
-		    console.dir("Error getting voucher pots");
-		    console.dir(error);
-		});
-	})		
-});
-
-//Fetch email templates
-app.get("/dataextension/lookup/templates", (req, res, next) => {
-
-	getOauth2Token().then((tokenResponse) => {
-
-	   	axios({
-			method: 'post',
-			url: templatesUrl,
-			headers: {'Authorization': tokenResponse},
-			data: templatePayload
-		})
-		.then(function (response) {
-			//console.dir(response.data);
-			res.json(response.data);
-		})
-		.catch(function (error) {
-			console.dir(error);
-			return error;
-		});
-	})	
-
-});
 
 function buildAssociationPayload(payload, incrementData, numberOfCodes) {
 	var campaignPromotionAssociationData = {};
@@ -485,202 +327,10 @@ function buildAssociationPayload(payload, incrementData, numberOfCodes) {
 	return campaignPromotionAssociationData;
 }
 
-function buildCommunicationCellPayload(payload) {
-	var communicationCellData = {
-			"not_control": {
-		    	"cell_code"					: payload["cell_code"],
-		    	"cell_name"					: payload["cell_name"],
-		        "campaign_name"				: payload["campaign_name"],
-		        "campaign_id"				: payload["campaign_id"],
-		        "campaign_code"				: payload["campaign_code"],
-		        "cell_type"					: "1",
-		        "channel"					: "2",
-		        "is_putput_flag"			: "1"				
-			},
-			"control": {
-		    	"cell_code"					: payload["cell_code"],
-		    	"cell_name"					: payload["cell_name"],
-		        "campaign_name"				: payload["campaign_name"],
-		        "campaign_id"				: payload["campaign_id"],
-		        "campaign_code"				: payload["campaign_code"],
-		        "cell_type"					: "2",
-		        "channel"					: "2",
-		        "is_putput_flag"			: "0"				
-			}
-	};
-	console.dir(communicationCellData);
-	return communicationCellData;
-}
-function buildPromotionDescriptionPayload(payload, incrementData, numberOfCodes) {
-	console.dir("payload is:");
-	console.dir(payload);
-	console.dir("increment payload");
-	console.dir(incrementData);
-	console.dir("number of codes: ");
-	console.dir(numberOfCodes);
-
-	var onlineTicker = 1;
-	var instoreTicker = 1;
-	var ticker = 1;
-	var commCellForPromo = incrementData.communication_cell_code_id_increment;
-
-	console.dir("comm cell id in desc build is:");
-	console.dir(commCellForPromo)
-
-	var promotionDescriptionData = {};
-	
-	promotionDescriptionData["promotions"] = {};
-
-	for ( var i = 1; i <= numberOfCodes; i++ ) {
-		var promotionArrayKey = "promotion_" + ticker;
-		console.dir("Promo ticker is promotion_" + ticker);
-		console.dir("I is " + i);
-
-		if ( payload.promotionType == "online" || payload.promotionType == "online_instore" ) {
-
-			console.dir("Promotion Type is " + payload.promotionType);
-			console.dir("in online mode");
-			if ( payload["global_code_" + onlineTicker] != "no-code" || payload["unique_code_" + onlineTicker] != "no-code" ) {
-				console.dir("ADDING ONLINE DATA");
-				promotionDescriptionData.promotions[promotionArrayKey] = {};
-				promotionDescriptionData.promotions[promotionArrayKey]["offer_channel"] 		= "Online";
-				promotionDescriptionData.promotions[promotionArrayKey]["offer_description"] 	= payload.campaign_name;
-				promotionDescriptionData.promotions[promotionArrayKey]["ts_and_cs"] 			= "-";
-				promotionDescriptionData.promotions[promotionArrayKey]["print_at_till_flag"] 	= payload.print_at_till_online;
-				promotionDescriptionData.promotions[promotionArrayKey]["instant_win_flag"] 		= payload.instant_win_online;
-				promotionDescriptionData.promotions[promotionArrayKey]["offer_medium"] 			= payload.offer_medium_online;
-				promotionDescriptionData.promotions[promotionArrayKey]["communication_cell_id"] = parseInt(commCellForPromo) + 1;
-				if ( payload.onlinePromotionType == "global" ) {
-					promotionDescriptionData.promotions[promotionArrayKey]["barcode"] 				= payload["global_code_" + onlineTicker];
-					promotionDescriptionData.promotions[promotionArrayKey]["promotion_id"] 			= payload["global_code_" + onlineTicker +"_promo_id"];
-					promotionDescriptionData.promotions[promotionArrayKey]["promotion_group_id"] 	= payload["global_code_" + onlineTicker +"_promo_group_id"];
-					promotionDescriptionData.promotions[promotionArrayKey]["valid_from_datetime"] 	= payload["global_code_" + onlineTicker +"_valid_from"];
-					promotionDescriptionData.promotions[promotionArrayKey]["valid_to_datetime"] 	= payload["global_code_" + onlineTicker +"_valid_to"];
-					promotionDescriptionData.promotions[promotionArrayKey]["visiblefrom"] 			= payload["global_code_" + onlineTicker +"_valid_from"];
-					promotionDescriptionData.promotions[promotionArrayKey]["visibleto"] 			= payload["global_code_" + onlineTicker +"_valid_to"];
-				} else if (payload.onlinePromotionType == "unique" ) {
-					promotionDescriptionData.promotions[promotionArrayKey]["barcode"] 				= "-";
-					promotionDescriptionData.promotions[promotionArrayKey]["promotion_id"] 			= payload["unique_code_" + onlineTicker +"_promo_id"];
-					promotionDescriptionData.promotions[promotionArrayKey]["promotion_group_id"] 	= payload["unique_code_" + onlineTicker +"_promo_group_id"];
-				}
-				onlineTicker++;
-				ticker++;
-			}
-		}
-		if ( payload.promotionType == "instore" || payload.promotionType == "online_instore" ) {
-			var promotionArrayKey = "promotion_" + ticker;
-			console.dir("Promo ticker is promotion_" + ticker);
-			console.dir("Promotion Type is " + payload.promotionType);
-			console.dir("In instore mode");
-			if ( payload["instore_code_" + instoreTicker] != "no-code" ) {
-				console.dir("ADDING INSTORE DATA");
-				promotionDescriptionData.promotions[promotionArrayKey] = {};
-				promotionDescriptionData.promotions[promotionArrayKey]["offer_channel"] 				= "Instore";
-				promotionDescriptionData.promotions[promotionArrayKey]["offer_description"] 			= payload.campaign_name;
-				promotionDescriptionData.promotions[promotionArrayKey]["ts_and_cs"] 					= "-";
-				promotionDescriptionData.promotions[promotionArrayKey]["barcode"] 						= payload["instore_code_" + instoreTicker];
-				promotionDescriptionData.promotions[promotionArrayKey]["promotion_id"]					= payload["instore_code_" + instoreTicker +"_promo_id"];
-				promotionDescriptionData.promotions[promotionArrayKey]["promotion_group_id"]			= payload["instore_code_" + instoreTicker +"_promo_group_id"];
-				promotionDescriptionData.promotions[promotionArrayKey]["valid_from_datetime"] 			= payload["instore_code_" + instoreTicker +"_valid_from"];
-				promotionDescriptionData.promotions[promotionArrayKey]["valid_to_datetime"] 			= payload["instore_code_" + instoreTicker +"_valid_to"];
-				promotionDescriptionData.promotions[promotionArrayKey]["visiblefrom"]					= payload["instore_code_" + instoreTicker +"_valid_from"];
-				promotionDescriptionData.promotions[promotionArrayKey]["visibleto"] 					= payload["instore_code_" + instoreTicker +"_valid_to"];
-				promotionDescriptionData.promotions[promotionArrayKey]["number_of_redemptions_allowed"] = payload["instore_code_" + instoreTicker +"_redemptions"];
-				promotionDescriptionData.promotions[promotionArrayKey]["print_at_till_flag"] 			= payload.print_at_till_instore;
-				promotionDescriptionData.promotions[promotionArrayKey]["instant_win_flag"] 				= payload.instant_win_instore;
-				promotionDescriptionData.promotions[promotionArrayKey]["offer_medium"] 					= payload.offer_medium_instore;
-				promotionDescriptionData.promotions[promotionArrayKey]["communication_cell_id"] 		= parseInt(commCellForPromo) + 1;
-				instoreTicker++;
-				ticker++;
-			}
-		}
-	}
-
-	console.dir(promotionDescriptionData);
-
-	return promotionDescriptionData;
-}
-
-function countCodes(payload) {
-
-	var cpaData = {};
-	for ( var i = 0; i < payload.length; i++ ) {
-		//console.dir("Step is: " + payload[i].step + ", Key is: " + payload[i].key + ", Value is: " + payload[i].value + ", Type is: " + payload[i].type);
-		cpaData[payload[i].key] = payload[i].value;
-	}
-
-	console.dir("payload passed to count");
-	console.dir(cpaData);
-
-	console.dir("get code as example");
-	console.dir(cpaData.global_code_1);
-
-	console.dir("get code as example dynamically");
-	console.dir(cpaData["global_code_1"]);
-
-	var globalCodes = 0;
-	var uniqueCodes = 0;
-	var instoreCodes = 0;
-	var totalCodes = 0;
-
-	for ( var i = 1; i <= 5; i++) {
-		if ( cpaData["global_code_" + i] != "no-code" ) {
-			globalCodes++;
-		}
-	}
-	for ( var i = 1; i <= 5; i++) {
-		if ( cpaData["unique_code_" + i] != "no-code" ) {
-			uniqueCodes++;
-		}
-	}
-	for ( var i = 1; i <= 5; i++) {
-		if ( cpaData["instore_code_" + i] != "no-code" ) {
-			instoreCodes++;
-		}
-	}
-	console.dir("num of codes:");
-	console.dir(parseInt(globalCodes) + parseInt(uniqueCodes) + parseInt(instoreCodes));
-	return parseInt(globalCodes) + parseInt(uniqueCodes) + parseInt(instoreCodes);
-}
-
-async function buildAndSend(payload) {
-	try {
-		const numberOfCodes = await countCodes(payload);
-		const incrementData = await getIncrements();
-		const associationPayload = await buildAssociationPayload(payload, incrementData, numberOfCodes);
-		const communicationCellPayload = await buildCommunicationCellPayload(associationPayload);
-		const promotionDescriptionPayload = await buildPromotionDescriptionPayload(associationPayload, incrementData, numberOfCodes);
-		const promotionObject = await saveToDataExtension(campaignAssociationUrl, associationPayload, incrementData.promotion_key, "cpa", "promotion_key");
-		const communicationCellObject = await saveToDataExtension(communicationCellUrl, communicationCellPayload, incrementData.communication_cell_code_id_increment, "communication_cell", "communication_cell_id");
-		const mcUniquePromotionObject = await saveToDataExtension(descriptionUrl, promotionDescriptionPayload, incrementData.mc_unique_promotion_id_increment, "promotion_description", "mc_unique_promotion_id");
-		await updateIncrements(updateIncrementsUrl, promotionObject, communicationCellObject, mcUniquePromotionObject, numberOfCodes);
-		return associationPayload;
-	} catch(err) {
-		console.dir(err);
-	}
-}
-
-async function sendBackPayload(payload) {
-	try {
-		await buildAndSend(payload);
-		const getIncrementsForSendback = await getIncrements();
-		var sendBackPromotionKey = parseInt(getIncrementsForSendback.promotion_key) + 1;
-		return sendBackPromotionKey
-	} catch(err) {
-		console.dir(err);
-	}
-
-}
 // insert data into data extension
 app.post('/dataextension/add/', async function (req, res){ 
 	console.dir("Dump request body");
 	console.dir(req.body);
-	try {
-		const nextKey = await sendBackPayload(req.body)
-		res.send(JSON.stringify(nextKey));
-	} catch(err) {
-		console.dir(err);
-	}
 	
 });
 
